6de5269141e30d00849cdac9e85e548637c5b463
sghosalkar <shubhamghosalkar.sg@gmail.com>
2019-12-01 05:02:17 +0530

    Add data simulation

--- pythonAPI/app/database/database_service.py
+++ pythonAPI/app/database/database_service.py
@@ -5,7 +5,7 @@ import requests
 def get_data(node_id):
     # API call to database service
     node_data_access_url = constants.NODE_URL + '/' + node_id
-    request_data = requests.get(node_data_access_url)
+    # request_data = requests.get(node_data_access_url)
 
     data = []
     for i in range(1, 10):
