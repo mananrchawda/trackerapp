a71b1c48b97e9f3aae020230336f54bf8fa45a50
manan <mchawda86@gmail.com>
2019-11-30 14:28:46 +0530

    Added mongo connection

--- pythonAPI/app/database/database_service.py
+++ pythonAPI/app/database/database_service.py
@@ -1,15 +1,21 @@
 from app.database import constants
 import requests
+import pymongo
 
 
 def get_data(node_id):
     # API call to database service
-    node_data_access_url = constants.NODE_URL + '/' + node_id
-    request_data = requests.get(node_data_access_url)
+    # node_data_access_url = constants.NODE_URL + '/' + node_id
 
-    data = []
-    for i in range(1, 10):
-        data.append(i * int(node_id))
-    return data
+    client = pymongo.MongoClient("mongodb+srv://density:density@cluster0-cvcng.gcp.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.test
+
+
+    # request_data = requests.get(node_data_access_url)
+    #
+    # data = []
+    # for i in range(1, 10):
+    #     data.append(i * int(node_id))
+    # return data
 
 
